12625af1a9b1b306d1086e3aa99069b4a4ca0682
Dillon O'Connor <dilong818@gmail.com>
2021-06-10 23:46:15 -0400

    implement nested attributes in booking form to simultaneously create booking and passenger records

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,14 +1,27 @@
 class BookingsController < ApplicationController
   def new
     @flight = Flight.find(params[:flight_id])
+    @passengers = params[:passenger_count].to_i
     @booking = Booking.new
+    @passengers.times { @booking.build_passenger }
   end
 
   def create
-    Passenger.create!(name: params[:booking][:name], email: params[:booking][:email])
-    @email = params[:booking][:email]
-    @flight_id = params[:booking][:flight]
-    PassengerMailer.with(email: @email, flight_id: @flight_id).ticket_email.deliver_later
-    redirect_to root_url, notice: "Check your email for your purchase confirmation."
+    @flight = Flight.find(params[:flight_id])
+    @email = params.dig(:booking, :passenger_attributes, :email)
+    @booking = @flight.bookings.new(booking_passenger_params)
+    if @booking.save
+      PassengerMailer.with(email: @email, flight_id: @flight.id).ticket_email.deliver_later
+      redirect_to root_url, notice: "Check your email for your purchase confirmation."
+    else
+      render :new
+    end
+  end
+
+
+  private
+
+  def booking_passenger_params
+    params.require(:booking).permit(:flight, passenger_attributes: [:name, :email])
   end
 end
