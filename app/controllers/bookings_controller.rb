6230ea5ceca5bd3daf42561c0e715b25f92d2d00
Dillon O'Connor <dilong818@gmail.com>
2021-06-11 21:20:16 -0400

    fix passenger name in mailer

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -8,10 +8,11 @@ class BookingsController < ApplicationController
 
   def create
     @flight = Flight.find(params[:flight_id])
-    @email = params.dig(:booking, :passenger_attributes, :email)
+    @passenger_name = params.dig(:booking, :passenger_attributes, :name)
+    @passenger_email = params.dig(:booking, :passenger_attributes, :email)
     @booking = @flight.bookings.new(booking_passenger_params)
     if @booking.save
-      PassengerMailer.with(email: @email, flight_id: @flight.id).ticket_email.deliver_later
+      PassengerMailer.with(passenger_name: @passenger_name, passenger_email: @passenger_email, flight_id: @flight.id).ticket_email.deliver_later
       redirect_to root_url, notice: "Check your email for your purchase confirmation."
     else
       render :new
--- app/mailers/passenger_mailer.rb
+++ app/mailers/passenger_mailer.rb
@@ -1,12 +1,14 @@
 class PassengerMailer < ApplicationMailer
   def ticket_email
-    @passenger = Passenger.find_by(email: params[:email])
-    @email = params[:email]
+    @passenger_name = params[:passenger_name]
+    @passenger_email = params[:passenger_email]
+
     @flight = Flight.find(params[:flight_id])
     @origin = Airport.find(@flight.origin).code
     @destination = Airport.find(@flight.destination).code
     @takeoff = @flight.takeoff_date_formatted
     @duration = @flight.pretty_duration
-    mail(to: @email, subject: "Ticket Confirmation")
+
+    mail(to: @passenger_email, subject: "Ticket Confirmation")
   end
 end
